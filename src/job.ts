f7f0f6b2354a510bde2514710c33df77d6ef0dab
Ryan Rampersad <ryan.rampersad@gmail.com>
2023-10-29 20:41:00 -0500

    Adds second arg, type

--- src/job.ts
+++ src/job.ts
@@ -7,12 +7,15 @@ import {
 import { getZoneId, getZoneRecordName } from "./env";
 import { getExternalIp } from "./ip";
 
+type OnUpdateFn = (ip: string, action: "same" | "updated") => void;
+// handleDiscordWebhook
+
 /**
  * Cron Job.
  *
  * @param onUpdate a function that runs when the patch runs, or when the patch is skipped
  */
-export async function job(onUpdate?: (ip: string) => void) {
+export async function job(onUpdate?: OnUpdateFn) {
   const zoneId = getZoneId();
   const zoneRecordName = getZoneRecordName();
 
@@ -26,9 +29,14 @@ export async function job(onUpdate?: (ip: string) => void) {
   const selectedRecord = selectZoneRecordByName(data.result, zoneRecordName);
   const selectedRecordZoneId = selectedRecord.id;
 
+  console.info(`🌤️ cloudflare reports the ip is ${selectedRecord.content}`);
+  console.info(
+    `🌤️ cloudflare reports the ip was set on ${selectedRecord.modified_on}`
+  );
+
   if (ip.ip === selectedRecord.content) {
     if (onUpdate) {
-      onUpdate(selectedRecord.content);
+      onUpdate(selectedRecord.content, "same");
     }
     console.info(`💸 exiting early; the ip is the same so no need to update`);
     return;
@@ -45,8 +53,8 @@ export async function job(onUpdate?: (ip: string) => void) {
   await patchDnsRecord(zoneId, selectedRecordZoneId, body);
 
   if (onUpdate) {
-    onUpdate(ip.ip);
+    onUpdate(ip.ip, "updated");
   }
 
-  console.info(`🌤 all done for now`);
+  console.info(`👍 all done for now`);
 }
